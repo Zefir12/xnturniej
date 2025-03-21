9415dedc061e1b777f7b0601a127b97627f83d11
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2025-03-21 01:18:29 +0100

    feat(ui): new colors, only dark mode, and live badges

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,28 @@ import App from './App.vue'
 import router from './router'
 import Aura from '@primeuix/themes/aura'
 import Tooltip from 'primevue/tooltip'
+import { definePreset } from '@primeuix/themes'
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{amber.50}',
+            100: '{amber.100}',
+            200: '{amber.200}',
+            300: '{amber.300}',
+            400: '{amber.400}',
+            500: '{amber.500}',
+            600: '{amber.600}',
+            700: '{amber.700}',
+            800: '{amber.800}',
+            900: '{amber.900}',
+            950: '{amber.950}',
+        },
+        formField: {
+            hoverBorderColor: '{primary.color}',
+        },
+    },
+})
 
 const app = createApp(App)
 
@@ -16,10 +38,8 @@ app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
-        options: {
-            darkModeSelector: true,
-        },
+        preset: MyPreset,
+        dark: true,
     },
 })
 
--- src/stores/mainStore.ts
+++ src/stores/mainStore.ts
@@ -4,10 +4,16 @@ import { getEventdata, getPlayerData } from '@/services/chessService'
 import { PlayerAccounts, uuidToPlayer } from '@/common/consts'
 import type { PlayerStats } from '@/models/models'
 import type { EventDataDto, PlayerRankingDto } from '@/models/dto'
+import { getAllPlayers } from '@/common/helpers.js'
+import { getStreamerStatus } from '@/services/kickService.js'
+import { isValid } from 'date-fns'
+import { getStreamerTwitchData } from '@/services/twitchService'
 
 export const useMainStore = defineStore('main', () => {
     const players = ref<PlayerRankingDto[]>([])
     const events = ref<EventDataDto>()
+    const kickStreamers = ref<{ live: boolean; game: string; player: string }[]>([])
+    const twitchStreamers = ref<{ live: boolean; game: string; uuid: string }[]>([])
 
     const getPlayerDataFromServer = async () => {
         const data = await getPlayerData()
@@ -20,6 +26,26 @@ export const useMainStore = defineStore('main', () => {
         players.value = [...playerList]
     }
 
+    const getStreamersStatusFromAPI = async () => {
+        const players = getAllPlayers()
+        const promises = players
+            .filter((player) => player.kick) // Only include players with a defined kick URL
+            .map((player) =>
+                getStreamerStatus(player.kick?.replace('https://kick.com/', '') ?? '').then((data) => ({
+                    live: data.isLive,
+                    game: data.game,
+                    player: player.name,
+                })),
+            )
+        const results = await Promise.all(promises)
+        kickStreamers.value = results
+    }
+
+    const getTwitchStreamerStatusFromServer = async () => {
+        const data = await getStreamerTwitchData()
+        twitchStreamers.value = data
+    }
+
     const getEventDataFromServer = async () => {
         const data = await getEventdata()
         events.value = data
@@ -28,9 +54,13 @@ export const useMainStore = defineStore('main', () => {
     onMounted(async () => {
         await getPlayerDataFromServer()
         await getEventDataFromServer()
+        await getTwitchStreamerStatusFromServer()
+        await getStreamersStatusFromAPI()
     })
     return {
         players,
         events,
+        kickStreamers,
+        twitchStreamers,
     }
 })
