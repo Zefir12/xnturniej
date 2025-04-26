2a8211bce4b97b44509835cf8f9b4a48c5aa14b2
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2025-04-26 16:53:54 +0200

    fix

--- src/components/PickEmComponents/LaddersPage.vue
+++ src/components/PickEmComponents/LaddersPage.vue
@@ -1,11 +1,17 @@
 <template>
     <CountDownTimer
+        v-if="expirationDates.laddersClose.getTime() > Date.now()"
         text="Możliwość wyboru zablokuje się za:"
         :date="expirationDates.laddersClose"
         rectColor="#222222"
     />
-    <br /><br />
-    <div :style="{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1rem' }">
+    <br v-if="expirationDates.laddersClose.getTime() > Date.now()" /><br
+        v-if="expirationDates.laddersClose.getTime() > Date.now()"
+    />
+    <div
+        v-if="expirationDates.laddersClose.getTime() > Date.now()"
+        :style="{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1rem' }"
+    >
         <StyledButton @click="saveLadder" :disabled="!changes">Zapisz</StyledButton
         ><StyledButton @click="resetLadders">Reset</StyledButton>
     </div>
@@ -16,24 +22,32 @@
         <img :src="CrownIcon" :style="{ width: '20px', height: '20px', marginTop: '16px', marginLeft: '10px' }" />
     </div>
 
-    <div class="main-container" :style="{ position: 'relative' }">
+    <div
+        class="main-container"
+        :class="{ 'no-select': expirationDates.laddersClose.getTime() < Date.now() }"
+        :style="{ position: 'relative' }"
+    >
         <div class="column" :style="{ display: 'flex', flexDirection: 'column', gap: '1rem' }">
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 v-model="ladderW.a"
                 a="a869eba0-ad12-11eb-b429-1d193d279471"
                 b="98f224b0-998c-11eb-9cd5-abe66a43618c"
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 v-model="ladderW.b"
                 a="d56506e2-747d-11ed-b280-8d2bac408b0d"
                 b="dd3a3172-ee19-11ef-84e3-1d869f158093"
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 v-model="ladderW.c"
                 a="2eb6a6f0-3df4-11eb-b13f-edcfbaf98d7b"
                 b="071f3ac4-5f0c-11eb-828b-1768796e906d"
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 v-model="ladderW.d"
                 a="8a2269ea-3dc0-11eb-8750-c5b5bc9f99f1"
                 b="5177ad7a-3e3e-11eb-b1e4-4fc24844be0f"
@@ -41,6 +55,7 @@
         </div>
         <div class="column" :style="{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }">
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 text="Półfinał"
                 :a="ladderW.a"
                 :b="ladderW.b"
@@ -48,6 +63,7 @@
                 v-model:lost="ladderW.l1"
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 text="Półfinał"
                 :a="ladderW.c"
                 :b="ladderW.d"
@@ -60,6 +76,7 @@
             :style="{ display: 'flex', flexDirection: 'column', gap: '2rem', justifyContent: 'center' }"
         >
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 text="Finał"
                 :a="ladderW.w1"
                 :b="ladderW.w2"
@@ -67,6 +84,7 @@
                 v-model:lost="ladderW.second"
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 text="3 miejsce"
                 :a="ladderW.l1"
                 :b="ladderW.l2"
@@ -95,26 +113,32 @@
         </div>
     </div>
     <h2 :style="{ marginBottom: '2rem', marginTop: '6rem', color: 'orange' }">Drabinka przegranych</h2>
-    <div class="main-container" :style="{ position: 'relative' }">
+    <div
+        class="main-container"
+        :class="{ 'no-select': expirationDates.laddersClose.getTime() < Date.now() }"
+        :style="{ position: 'relative' }"
+    >
         <div class="column" :style="{ display: 'flex', flexDirection: 'column', gap: '1rem' }">
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 v-model="ladderL.a"
                 a="59482f96-4e7b-11eb-b365-3d75dc4dac27"
                 b="c49386f0-a924-11ed-88da-cd5896a24560"
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 v-model="ladderL.b"
                 a="661e726e-b17c-11ed-97ad-175efc8ca79c"
                 b="b68f03f2-41ef-11eb-b636-9ddfe076e3d4"
-                disabled
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 v-model="ladderL.c"
                 a="9182bba4-8cec-11ed-9f28-a1a555fdd6af"
                 b="7814e9b0-410a-11eb-8e01-ef8706c45d64"
-                disabled
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 v-model="ladderL.d"
                 a="97709332-e8df-11ef-ae1f-699fe3713de1"
                 b="cc9cbaec-71fe-11eb-b3f6-4b737133070f"
@@ -122,6 +146,7 @@
         </div>
         <div class="column" :style="{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }">
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 text="Półfinał"
                 :a="ladderL.a"
                 :b="ladderL.b"
@@ -129,6 +154,7 @@
                 v-model:lost="ladderL.l1"
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 text="Półfinał"
                 :a="ladderL.c"
                 :b="ladderL.d"
@@ -141,6 +167,7 @@
             :style="{ display: 'flex', flexDirection: 'column', gap: '2rem', justifyContent: 'center' }"
         >
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 text="Finał"
                 :a="ladderL.w1"
                 :b="ladderL.w2"
@@ -148,6 +175,7 @@
                 v-model:lost="ladderL.second"
             />
             <LadderSelectPlayerBox
+                :disabled="expirationDates.laddersClose.getTime() < Date.now()"
                 text="11 miejsce"
                 :a="ladderL.l1"
                 :b="ladderL.l2"
@@ -295,7 +323,9 @@ watch(
 
         // Set a new timeout to save the ladder after 3 seconds
         saveTimeout = setTimeout(() => {
-            saveLadder() // Call saveLadder after 3 seconds
+            if (expirationDates.laddersClose.getTime() > Date.now()) {
+                saveLadder() // Call saveLadder after 3 seconds
+            }
         }, 6000) // 3000 ms (3 seconds)
     },
     { immediate: false, deep: true },
@@ -317,6 +347,9 @@ watch(
 )
 
 const saveLadder = async () => {
+    if (expirationDates.laddersClose.getTime() < Date.now()) {
+        return
+    }
     changes.value = false
     const result = await api.post('/pickem/chooseladder', {
         id: JSON.parse(localStorage.getItem('pickemTwitchUser') ?? '{}')._id,
@@ -345,6 +378,13 @@ const saveLadder = async () => {
 }
 </script>
 <style scoped>
+.no-select {
+    user-select: none;
+    pointer-events: none;
+    -webkit-user-select: none; /* For Safari */
+    -moz-user-select: none; /* For Firefox */
+    -ms-user-select: none; /* For Internet Explorer/Edge */
+}
 .main-container {
     width: 900px;
     display: flex;
