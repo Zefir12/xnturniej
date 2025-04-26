b50e9cbb0185a0e513287fb1c93d2d09af7a8356
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2025-04-26 10:19:55 +0200

    fix

--- src/components/PickEmComponents/LaddersPage.vue
+++ src/components/PickEmComponents/LaddersPage.vue
@@ -175,7 +175,7 @@
         </div>
     </div>
     <p :style="{ color: 'orange', fontSize: '12px', marginTop: '8rem' }">
-        Drabinka może pozwolić picknąć np. dwie kasie do połfinałów, trzeba to narazie niestety ręcznie poprawić, prosze
+        Drabinka może pozwolić picknąć np. dwie Kasie do połfinałów, trzeba to narazie niestety ręcznie poprawić, prosze
         uważać i pamiętać o zapisywaniu, dane są bezpieczne jak zapiszecie, a ja będe poprawiał wygląd drabinek w
         międzyczasie<br /><span :style="{ fontWeight: 'bold' }">Punktacja: </span>
         <span :style="{ fontWeight: 'bold' }">+3pkt</span> za poprawny pick w ćwierćfinale,
