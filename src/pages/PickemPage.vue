eeaf6798c244504b33ce4abe60b4f49ab8258ea3
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2025-04-27 15:11:50 +0200

    fix

--- src/pages/PickemPage.vue
+++ src/pages/PickemPage.vue
@@ -106,7 +106,7 @@
                     </div>
                     <div>
                         🏆
-                        {{ userStore.playerData?.total_points ?? 0 }}
+                        {{ (userStore.playerData?.group_points ?? 0) + (userStore.playerData?.ladder_points ?? 0) }}
                         punktów
                     </div>
                 </div>
